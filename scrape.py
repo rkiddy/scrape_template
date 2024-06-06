348b169ec8224c6ace8322f63f76c7daebf2129a
Ray Kiddy <ray@ganymede.org>
2024-06-06 14:38:11 -0700

    Set user-agent on scrape.

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ from selenium.webdriver.firefox.options import Options
 
 opts = Options()
 opts.add_argument("--headless")
+opts.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"')
 
 br = webdriver.Firefox(options=opts)
 
@@ -15,7 +16,8 @@ br.set_window_size(900, 900)
 
 br.implicitly_wait(10)
 
-url = 'https://procurement.sccgov.org/doing-business-county/active-contracts'
+# url = 'https://procurement.sccgov.org/doing-business-county/active-contracts'
+url = 'https://justicethomasvacationclub.com'
 
 br.get(url)
 
