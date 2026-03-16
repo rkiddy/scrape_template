713806fe2fe20c016c8f264ba4de0b797c8de75d
Ray Kiddy <ray@ganymede.org>
2026-03-16 09:55:17 -0700

    restructure as I usually do.

--- scrape_chrome.py
+++ scrape_chrome.py
@@ -2,42 +2,48 @@ import time
 import traceback
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-opts = Options()
-opts.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " \
+
+def browser():
+    opts = Options()
+    opts.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 " \
                   "(KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"')
-opts.add_argument("--disable-gpu")
-opts.add_argument("--no-sandbox")
-opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--disable-dev-shm-usage")
 
-br = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+    br = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
 
-br.set_window_position(50, 50)
-br.set_window_size(900, 900)
+    br.set_window_position(50, 50)
+    br.set_window_size(900, 900)
 
-br.implicitly_wait(10)
+    br.implicitly_wait(10)
+    return br
 
-url = 'https://www.google.com'
 
-br.get(url)
+if __name__ == '__main__':
 
-print("Good to go!")
+    br = browser()
 
-time.sleep(5)
+    url = 'https://www.google.com'
 
-try:
+    br.get(url)
 
-    for link in br.find_elements(By.TAG_NAME, 'a'):
-        print(f"    link: {link.get_attribute('href')}")
+    print("Good to go!")
 
-except:
-    traceback.print_exc()
-finally:
-    br.quit()
+    time.sleep(5)
 
-print("OK!")
+    try:
+        for link in br.find_elements(By.TAG_NAME, 'a'):
+            print(f"    link: {link.get_attribute('href')}")
+    except:
+        traceback.print_exc()
+    finally:
+        br.quit()
+
+    print("OK!")
+
--- scrape_firefox.py
+++ scrape_firefox.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
+
+def browser():
 opts = Options()
 opts.add_argument("--headless")
 opts.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"')
@@ -15,24 +17,26 @@ br.set_window_position(50, 50)
 br.set_window_size(900, 900)
 
 br.implicitly_wait(10)
+    return br
 
-# url = 'https://procurement.sccgov.org/doing-business-county/active-contracts'
-url = 'https://justicethomasvacationclub.com'
 
-br.get(url)
+if __name__ == '__main__':
 
-print("Good to go!")
+    url = 'https://opencalaccess.org'
 
-time.sleep(5)
+    br.get(url)
 
-try:
+    print("Good to go!")
 
-    for link in br.find_elements(By.TAG_NAME, 'a'):
-        print(f"    link: {link.get_attribute('href')}")
+    time.sleep(5)
 
-except:
-    traceback.print_exc()
-finally:
-    br.quit()
+    try:
+        for link in br.find_elements(By.TAG_NAME, 'a'):
+            print(f"    link: {link.get_attribute('href')}")
+    except:
+        traceback.print_exc()
+    finally:
+        br.quit()
 
-print("OK!")
+    print("OK!")
+
